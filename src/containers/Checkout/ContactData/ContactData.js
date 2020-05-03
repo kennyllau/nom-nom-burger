90a6a86852c1cd96ad841922289c57a06a9f5ded
Kenny Lau <K.Lau@apple.com>
2020-05-02 21:39:45 -0700

    validating form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                         {value: 'fastest', displayValue: 'Fastest'},
                         {value: 'cheapest', displayValue: 'Cheapest'},
                     ]
-                }
+                },
+                value: 'Fastest'
             }
         },
         loading: false
@@ -32,25 +33,68 @@ class ContactData extends Component {
                 type: configType,
                 placeholder: placeholder
             },
-            value: ''
+            value: '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            minLength: 3
         }
     };
 
     orderHandler = (event) => {
 
         event.preventDefault();
-
         // this.setState({loading: true});
+
+        const formData = {};
+
+        for (let eleId in this.state.orderForm) {
+            formData[eleId] = this.state.orderForm[eleId].value;
+        }
 
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
         };
 
         // send to backend
         // axios.post();
-        console.log(this.props, "dasdsad");
-        this.props.history.push('/');
+        console.log(order, "dasdsad");
+        // this.props.history.push('/');
+    };
+
+    checkValidity(value, rules) {
+
+        let isValid = false;
+
+        if (rules.required) {
+            isValid = value.trim() !== '';
+        }
+
+        if (rules.minLength) {
+            console.log("asdasd");
+            isValid = value.length >= rules.minLength;
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+
+        const updatedOrderForm = {
+            ...this.state.orderForm // the nested obj is not deeped clone (still referenced)
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier] // this will be deep cloned (no nested obj)
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
+        this.setState({orderForm: updatedOrderForm});
     };
 
     render() {
@@ -65,9 +109,14 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
-                <Input key={formElement.id} elementType={formElement.config.elementType} elementConfig={formElement.config.elementConfig} value={formElement.config.value} />
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
